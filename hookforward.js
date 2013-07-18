f811b14ae2e0cc531575a3bf17ef7a3d670eaffe
Geoff Buesing <gbuesing@gmail.com>
2013-07-18 16:59:29 -0500

    Version 0.3.1. Fix bug with trying to load obsolete teshook module

--- hookforward.js
+++ hookforward.js
@@ -1,7 +1,5 @@
 var pusher = require('./pusher');
 var forwarder = require('./forwarder');
-var testhook = require('./testhook');
 
 module.exports = forwarder;
 module.exports.pushDesignDoc = pusher;
-module.exports.postTestHook = testhook;
